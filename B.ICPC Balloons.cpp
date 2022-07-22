c07ed4a53d39c7a0cf3d574ee31ad50090496d8e
Adarsh <adarshsingh2k@gmail.com>
2022-07-22 23:24:10 +0530

    Commit

---
+++ B.ICPC Balloons.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int solve(string s){
+    unordered_map<char>team;
+    for(int i=0;i<s.size();i++){
+            team[s[i]]++;
+    }
+    for(auto i:team){
+        if(i.find(i.second!=1))
+    }
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s; cin>>s;
+    }
+}
